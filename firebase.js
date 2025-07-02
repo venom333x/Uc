5b1e1a6b8ab58e3f3c5f09564015c6409f387a65
venom333x <rajatabish969@gmail.com>
2025-07-02 12:19:25 +0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9dMXMN13k1S8S3aBoKkC_tbtRiMMqA4",
+  authDomain: "midasbuy-uc10.firebaseapp.com",
+  databaseURL: "https://midasbuy-uc10-default-rtdb.firebaseio.com",
+  projectId: "midasbuy-uc10",
+  storageBucket: "midasbuy-uc10.appspot.com",
+  messagingSenderId: "455151617103",
+  appId: "1:455151617103:web:900ebfaba9f7b9c5be8aaa",
+  measurementId: "G-6B2SK9KQ72"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
